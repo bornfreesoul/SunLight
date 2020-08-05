276617a3ba05247025beccce6c782a4287a7fd23
Kapil <bornfreesoul@gmail.com>
2020-08-05 17:47:29 +0530

    JavaScript

---
+++ home.js
@@ -0,0 +1,16 @@
+console.log("Hello");
+// alert("Google is my next company");
+var k = "food@!";
+console.log(k);
+
+var sum = 45;
+console.log(sum);
+
+//var age = prompt("What is your age?");
+//document.getElementById("sim").innerHTML = age;
+
+var num1 = 20;
+num1 = num1 + 2;
+console.log(num1);
+
+
